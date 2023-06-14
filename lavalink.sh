e7398c17996774be8852f23faf07555edb52558c
thebublik2013 <127696326+thebublik2013@users.noreply.github.com>
2023-06-14 20:51:46 +0300

    Update lavalink.sh

--- lavalink.sh
+++ lavalink.sh
@@ -2,4 +2,3 @@
 MEMORY=$1
 rm -f ./Lavalink.jar
 curl -L -o Lavalink.jar https://github.com/lavalink-devs/Lavalink/releases/latest/download/Lavalink.jar
-java -Xms128M -Xmx${MEMORY}M -Dterminal.jline=false -Dterminal.ansi=true -jar ${JARFILE}
