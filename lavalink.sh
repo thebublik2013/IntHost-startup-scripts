24e02ff9e0b3530909fd1c918b74860f0e18f4ed
thebublik2013 <127696326+thebublik2013@users.noreply.github.com>
2023-06-14 20:54:30 +0300

    Update lavalink.sh

--- lavalink.sh
+++ lavalink.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rm -f ./Lavalink.jar
-curl -L -o Lavalink.jar https://github.com/lavalink-devs/Lavalink/releases/latest/download/Lavalink.jar
+curl -L -o IntHost.jar https://github.com/lavalink-devs/Lavalink/releases/latest/download/Lavalink.jar
