f45060de896c7e46c2d00185a1a4c8fe1a7452a1
thebublik2013 <127696326+thebublik2013@users.noreply.github.com>
2023-06-14 21:18:06 +0300

    Update lavalink.sh

--- lavalink.sh
+++ lavalink.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -f ./IntHost.jar
-printf  \033[1m\033[33mcontainer@IntHost~ \033[0m Installing lavalink \n
+printf  \033[1m\033[33mcontainer@IntHost~ \033[0m Installing lavalink\n
 curl -L -o IntHost.jar https://github.com/lavalink-devs/Lavalink/releases/latest/download/Lavalink.jar
-printf \033[1m\033[33mcontainer@IntHost~ \033[0m Starting lavalink \n
+printf \033[1m\033[33mcontainer@IntHost~ \033[0m Starting lavalink\n
 java -Xms128M -XX:MaxRAMPercentage=95.0 -Dterminal.jline=false -Dterminal.ansi=true -jar IntHost.jar
