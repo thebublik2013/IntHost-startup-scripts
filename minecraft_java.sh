017e9802df7e398306e413c1513886c2bfcb13d9
thebublik2013 <127696326+thebublik2013@users.noreply.github.com>
2023-06-14 22:57:43 +0300

    Update minecraft_java.sh

--- minecraft_java.sh
+++ minecraft_java.sh
@@ -2,7 +2,7 @@
 MEMORY=$1
 JARFILE=$2
 echo 'eula=true' > ./eula.txt
-rm -f ./plugins/anet.jar ./plugins/alaister_net_hibernate.jar ./plugins/Hibernate/config.yml
-curl --create-dirs -sL -o ./plugins/alaister_net_hibernate.jar https://api.spiget.org/v2/resources/4441/download
-curl --create-dirs -sL -o ./plugins/Hibernate/config.yml https://github.com/alaister-net/server-startup-scripts/raw/master/minecraft_java/config.yml
+rm -f ./plugins/IntHost_hibernate.jar ./plugins/Hibernate/config.yml
+curl --create-dirs -sL -o ./plugins/IntHost_hibernate.jar https://api.spiget.org/v2/resources/4441/download
+curl --create-dirs -sL -o ./plugins/Hibernate/config.yml https://github.com/thebublik2013/IntHost-startup-scripts/raw/main/hibirnate/config.yml
 java -Xms128M -Xmx${MEMORY}M -Dterminal.jline=false -Dterminal.ansi=true -jar ${JARFILE}
