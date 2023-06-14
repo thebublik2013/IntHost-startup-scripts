0ca27f8428ea6e5fdf06716afad334c2f84efd13
thebublik2013 <127696326+thebublik2013@users.noreply.github.com>
2023-06-14 21:45:58 +0300

    Update lavalink.sh

--- lavalink.sh
+++ lavalink.sh
@@ -5,5 +5,4 @@ curl -L -o IntHost.jar https://github.com/lavalink-devs/Lavalink/releases/latest
 echo Starting lavalink
 java -Xms128M -XX:MaxRAMPercentage=95.0 -Dterminal.jline=false -Dterminal.ansi=true -jar IntHost.jar &
 MyPID=$!                        # You sign it's PID
-echo "kill $MyPID" > lavalink.sh  # Write the the command kill pid in MyStop.sh
 rm -f ./lavalink.sh
